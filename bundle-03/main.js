a7737fa7ebbe6f147dce0a6b31907a9198dfc750
MatteoSanson <matteo.sanson@gmail.com>
2023-11-26 11:03:55 +0100

    feat: b3 es1

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -11,6 +11,84 @@
 
 
 // ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+// const cars = [
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Fiesta',
+//         type: 'diesel'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'A1',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Golf',
+//         type: 'Benzina'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Panda',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Multipla',
+//         type: 'GPL'
+//     },
+//     {
+//         manufacturer: 'Tesla',
+//         model: 'Model 3',
+//         type: 'elettrico'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Polo',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Kuga',
+//         type: 'Diesel'
+//     },
+//     {
+//         manufacturer: 'Seat',
+//         model: 'Ibiza',
+//         type: 'metano'
+//     }
+//     {
+//         manufacturer: 'Audi',
+//         model: 'R8',
+//         type: 'Benzina'
+//     },
+// ];
+
+// const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+
+// const dieselCars = cars.filter( (auto) => {
+//     auto.type === 'diesel';
+// });
+
+// const otherCars = cars.filter( (auto) => {
+//     return auto.type !== 'benzina' || auto.type !== 'diesel';
+// });
+
+// console.log('Auto a benzina');
+// console.log('*******************************');
+// console.log(gasolineCars);
+
+// console.log('Auto a diesel');
+// console.log('*******************************');
+// console.log(dieselCars);
+
+// console.log('Tutte le altre auto');
+// console.log(otherCars);
+
+
+// 1) abbiamo un array di oggetti cars, con 3 chiavi e valori stringa. nella variabile gasolineCars andiamo a filtrare l'array cars, creando un nuovo array, contenente solo le auto di tipo "benzina". La stessa cosa per la variabile dieselCars, filtriamo sull'array cars creando un nuovo array contenente solo le auto di tipo "diesel". Lo stesso per otherCars ma con le auto che NON sono "benzina" o "diesel". Vengono stampati in console questi nuovi array.
+// 2) e 3) all'interno dell'array di oggetti manca una virgola a separare gli ultimi due oggetti. Nella riga 67 c'è una funzione freccia scritta al contrario, la soluzione corretta è =>. Inoltre, dato che la tipologia è scritta con caratteri maiuscoli e minuscoli all'interno degli oggetti, per poterli filtrare meglio si potrebbe aggiungere ad auto.type il metodo toLowerCase() per fare il confronto con "benzina". A riga 69 e 71 ci sono delle parentes grafe in più e anche qui si può aggiungere il metoto toLowerCase(). A riga 74 anche qui aggiunto toLowerCase e sostiuamo l'operatore logico or || con and && per raggruppare entrambi i tipi. A seguito il codice corretto.
+
 const cars = [
     {
         manufacturer: 'Ford',
@@ -56,7 +134,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,23 +142,21 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
 
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
-});
+const dieselCars = cars.filter( (auto) => auto.type.toLowerCase() === 'diesel');
 
 const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel';
 });
 
 console.log('Auto a benzina');
-console.log('*******************************');
 console.log(gasolineCars);
+console.log('*******************************');
 
 console.log('Auto a diesel');
-console.log('*******************************');
 console.log(dieselCars);
+console.log('*******************************');
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
